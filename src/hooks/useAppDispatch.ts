a686ff6427f19fdedafb9a6429706e589f5488a7
GitHubermane <crykour12@gmail.com>
2023-06-06 16:53:56 +0300

    build

--- src/hooks/useAppDispatch.ts
+++
@@ -1,4 +0,0 @@
-import { useDispatch } from "react-redux";
-import { DispatchType } from "../store";
-
-export const useAppDispatch: () => DispatchType = useDispatch
--- src/types/messageType.ts
+++
@@ -1,5 +0,0 @@
-export type messageType = {
-    text: string
-    time: string
-    isSelf: boolean
-}
